d3b5812813676c38bd7a5dc9bd8bca9a0fcc92b3
atifkarim <atif0902082@gmail.com>
2020-12-06 22:07:40 +0100

    set to update from JSON file, shutdown server function added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from make_auto_csv import do_process, create_json, load_json
+from make_auto_csv import do_process, create_json, load_json, load_csv
 from flask import jsonify
 import os
 
@@ -24,16 +24,29 @@ def parse(name=None):
 @app.route('/auto_update')
 def parse_auto_update(name=None):
     data_json = load_json()
+    # print(data_json)
     return jsonify(data_json)
-    
 
 # running of python file using Flask is done without reloading HTML
 @app.route('/background_process_test')
 def background_process_test():
-    # do_process() # create csv file
-    create_json() # create json file
+    # do_process() # create csv file if no file in the directory. You can press the TEST button.
+    create_json() # create json file if no file in the directory. You can press the TEST button.
     return "nothing"
 
+# to stop flask server
+
+from flask import request
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 if __name__ == '__main__':
     app.run(host='192.168.0.74', port=5012)
