b42afd06cda76bad6c747dd30be08db17bd9ecd1
atifkarim <atif0902082@gmail.com>
2020-12-06 22:08:49 +0100

    trying to work with chart JS, canvas JS working finely

--- templates/static/multiple_line_chart_canvas_JS_json.js
+++ templates/static/multiple_line_chart_canvas_JS_json.js
@@ -24,7 +24,7 @@ function getDataPointsFromCSV_1(data) {
 // var chart = new CanvasJS.Chart("chartContainer_bottom_left");
 
 // $.get("https://raw.githubusercontent.com/atifkarim/Time-Series-Forecasting-Using-Machine-Learning-Algorithm/develop/csv_column_2.csv", function(data)
-var getData = function test() {
+var getData_json = function test() {
     $.get("/auto_update", function (data) {
         var chart = new CanvasJS.Chart("chartContainer_bottom_left", {
             title: {
@@ -45,3 +45,65 @@ var getData = function test() {
 
     });
 }
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////// DATA FROM CSV /////////////////////////////////////////////////////
+
+/*var dataPoints = [];
+
+function getDataPointsFromCSV(csv) {
+    var dataPoints = csvLines = points = [];
+    csvLines = csv.split(/[\r?\n|\r|\n]+/);
+
+    for (var i = 0; i < csvLines.length; i++)
+        if (csvLines[i].length > 0) {
+            points = csvLines[i].split(",");
+            dataPoints.push({
+                x: parseFloat(points[0]),
+                y: parseFloat(points[1])
+            });
+        }
+    return dataPoints;
+}
+
+function getDataPointsFromCSV_1(csv) {
+    var dataPoints = csvLines = points = [];
+    csvLines = csv.split(/[\r?\n|\r|\n]+/);
+
+    for (var i = 0; i < csvLines.length; i++)
+        if (csvLines[i].length > 0) {
+            points = csvLines[i].split(",");
+            dataPoints.push({
+                x: parseFloat(points[0]),
+                y: parseFloat(points[2])
+            });
+        }
+    return dataPoints;
+}
+// var chart = new CanvasJS.Chart("chartContainer_bottom_left");
+
+// $.get("https://raw.githubusercontent.com/atifkarim/Time-Series-Forecasting-Using-Machine-Learning-Algorithm/develop/csv_column_2.csv", function(data)
+var getData_csv = function test() {
+    $.get("/auto_update", function (data) {
+        var chart = new CanvasJS.Chart("chartContainer_bottom_left", {
+            title: {
+                text: "Chart from CSV",
+            },
+            data: [{
+                type: "line",
+                dataPoints: getDataPointsFromCSV(data)
+            },
+            {
+                type: "line",
+                dataPoints: getDataPointsFromCSV_1(data)
+            }
+            ]
+        });
+
+        chart.render();
+
+    });
+}
+*/
