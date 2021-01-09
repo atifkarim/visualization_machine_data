9c2231788dc2addf5630b4ba3834c6e158376a91
atifkarim <atif0902082@gmail.com>
2021-01-09 03:50:34 +0100

    add button(will be deprecated maybe)

--- templates/static/js/python_button.js
+++ templates/static/js/python_button.js
@@ -1,25 +1,37 @@
 // source https://stackoverflow.com/questions/21566649/flask-button-run-python-without-refreshing-page
 
-var python_button_function = function () {
+var python_button_function = function() {
     $(function() {
         $('a#test').bind('click', function() {
-          $.getJSON('/background_process_test',
-              function(data) {
-            //do nothing
-          });
-      return false;
+            $.getJSON('/background_process_test',
+                function(data) {
+                    //do nothing
+                });
+            return false;
+        });
     });
-  });
 };
 
-var stop_flask_server = function () {
-  $(function() {
-      $('a#shutdown').bind('click', function() {
-        $.getJSON('/shutdown',
-            function(data) {
-          //do nothing
+var update_write_function_for_table = function() {
+    $(function() {
+        $('a#write_function_update_for_table').bind('click', function() {
+            $.getJSON('/write_function_update_for_table',
+                function(data) {
+                    //do nothing
+                });
+            return false;
         });
-    return false;
-  });
-});
+    });
+};
+
+var stop_flask_server = function() {
+    $(function() {
+        $('a#shutdown').bind('click', function() {
+            $.getJSON('/shutdown',
+                function(data) {
+                    //do nothing
+                });
+            return false;
+        });
+    });
 };
