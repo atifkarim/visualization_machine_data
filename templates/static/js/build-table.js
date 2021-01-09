10c7549f1e9e4f7e818c8e49b4ed968a997253f6
atifkarim <atif0902082@gmail.com>
2021-01-09 03:50:55 +0100

    console.log removed

--- templates/static/js/build-table.js
+++ templates/static/js/build-table.js
@@ -1,36 +1,36 @@
 function createTable(caption, data) {
-   let table = $("<table class='test_table'>");
-   table.html("<caption>" + caption + "</caption>");
+    let table = $("<table class='test_table'>");
+    table.html("<caption>" + caption + "</caption>");
 
-   let headers = getColumns(data[Object.keys(data)[0]]);
-   let tr = $("<tr>");
-   for (let i in headers) {
-      tr.append("<th>" + headers[i] + "</th>");
-   }
-   table.append(tr);
+    let headers = getColumns(data[Object.keys(data)[0]]);
+    let tr = $("<tr>");
+    for (let i in headers) {
+        tr.append("<th>" + headers[i] + "</th>");
+    }
+    table.append(tr);
 
-   for (let row in data) {
-      let tr = $("<tr>");
-      for (let col in data[row]) {
-         let format_data = check_input_val_type(data[row][col], 1);
-         let td = $("<td>").text(format_data);
-         // let td = $("<td>").text(data[row][col]);
-         tr.append(td);
-      }
-      table.append(tr);
-   }
+    for (let row in data) {
+        let tr = $("<tr>");
+        for (let col in data[row]) {
+            let format_data = check_input_val_type(data[row][col], 1);
+            let td = $("<td>").text(format_data);
+            // let td = $("<td>").text(data[row][col]);
+            tr.append(td);
+        }
+        table.append(tr);
+    }
 
-   return table;
+    return table;
 }
 
 
 function getColumns(data) {
-   let headers = [];
-   for (let col in data) {
-      headers.push(col); 
-   }
+    let headers = [];
+    for (let col in data) {
+        headers.push(col);
+    }
 
-   return headers;
+    return headers;
 }
 
 
@@ -38,29 +38,27 @@ function getColumns(data) {
  * Check value is float or int or string and return value. IF ffloat then it will round to a give fixed point. You can omit it if not required
  */
 function check_input_val_type(y, fixed_range) {
-   if (parseFloat(y) && (Number(y) === y && y % 1 !== 0)) {
-      console.log("y is a float and val is: ", y, " and conv: ", y.toFixed(fixed_range));
-      y = y.toFixed(fixed_range);
-      return y;
-   }
-   else if (!parseFloat(y)) {
-      console.log("y is a string and val is: ", y);
-      return y;
-   }
-   else {
-      console.log("y is a int and val is: ", y);
-      return y;
-   }
+    if (parseFloat(y) && (Number(y) === y && y % 1 !== 0)) {
+        // console.log("y is a float and val is: ", y, " and conv: ", y.toFixed(fixed_range));
+        y = y.toFixed(fixed_range);
+        return y;
+    } else if (!parseFloat(y)) {
+        //   console.log("y is a string and val is: ", y);
+        return y;
+    } else {
+        //   console.log("y is a int and val is: ", y);
+        return y;
+    }
 }
 
 
 function getTableData_json() {
-   $.get("/auto_update_table", function (data) {
-      let tbl_div = $("#table_div");
-      tbl_div.html("");
-      for (let t in data) {
-         new_tbl = createTable("Table " + t, data[t]);
-         tbl_div.append(new_tbl);
-      }
-   });
+    $.get("/auto_update_table", function(data) {
+        let tbl_div = $("#table_div");
+        tbl_div.html("");
+        for (let t in data) {
+            new_tbl = createTable("Table " + t, data[t]);
+            tbl_div.append(new_tbl);
+        }
+    });
 }
