319a3f49432cf0a11ce9a3dfe2ce4a3a5ac25db0
atifkarim <atif0902082@gmail.com>
2021-01-10 00:21:11 +0100

    trying to update object in client side and pass to backend

--- templates/static/js/get_vars.js
+++ templates/static/js/get_vars.js
@@ -1,7 +1,7 @@
 let selectvar = document.getElementById("varselect");
 let varval = document.getElementById("varvalue");
 let dict = {};
-//let newdict = {};
+let newdict = {};
 
 let get_val = function() {
     let varname = selectvar.value;
@@ -22,22 +22,53 @@ let saveindict = function() {
 }
 */
 
+// let prepare = function(var_dict) {
+//     selectvar.innerHTML = '<option selected="true" disabled="disabled">Choose variable</option>';
+//     dict = var_dict;
+
+//     for ([key, value] of Object.entries(var_dict)) {
+//         let option = document.createElement("option");
+//         option.text = key;
+//         option.value = key;
+//         selectvar.add(option);
+//     }
+// }
+
+function updatedata() {
+    var variableToUpdate = $("#varselect :selected").text();
+    console.log("*** ", variableToUpdate);
+    var updatedValue = varval.value;
+    console.log("---- ", updatedValue);
+    newdict[variableToUpdate] = updatedValue;
+    console.log("update newdict: ", newdict);
+    // setdata(dataTest);
+}
+
+function createURL() {
+    let urlString = '/update_data_in_pyhton?';
+    for ([key, value] of Object.entries(newdict)) {
+        urlString = urlString + key + '=' + value + '&';
+    }
+    document.getElementById("createURL").innerHTML = urlString.slice(0, -1);
+}
+
 let prepare = function(var_dict) {
-    selectvar.innerHTML = '<option selected="true" disabled="disabled">Choose variable</option>';
-    dict = var_dict;
+
+    // selectvar.innerHTML =
+    '<option selected="true" disabled="disabled">Choose variable</option>';
 
     for ([key, value] of Object.entries(var_dict)) {
-        let option = document.createElement("option");
-        option.text = key;
-        option.value = key;
-        selectvar.add(option);
+        var daySelect = document.getElementById('varselect');
+        daySelect.options[daySelect.options.length] = new Option(key, value);
     }
-}
+};
 
 let get_dropdown = function() {
     $.getJSON('/update_dropdown',
         function(data) {
+            newdict = data;
             prepare(data);
+            varval.value = selectvar.value
         });
     return false;
 };
