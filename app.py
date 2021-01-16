c71c7f962f16e226ce000ad312c274858288d8fe
atifkarim <atif0902082@gmail.com>
2021-01-17 00:13:34 +0100

    auto ip address will fetch

--- app.py
+++ app.py
@@ -80,7 +80,18 @@ def shutdown():
     shutdown_server()
     return 'Server shutting down...'
 
+
+# get the ip address of the connection
+import socket
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
 if __name__ == '__main__':
-    app.run(host='192.168.0.74', port=5012)
+    ip_address_var = get_ip_address()
+    app.run(host=ip_address_var, port=5012)
+    # app.run(host='192.168.0.74', port=5012)
     # app.run(host='localhost', debug=True, port=5000)
     app.debug = True
