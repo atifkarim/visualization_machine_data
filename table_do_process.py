e63d4fa7de307eb728adb247e4d5ccd28fa13148
atifkarim <atif0902082@gmail.com>
2021-01-17 00:14:09 +0100

    random value for table gen added

--- table_do_process.py
+++ table_do_process.py
@@ -1,4 +1,8 @@
 from table_const_var import *
+from random import seed
+from random import gauss
+from random import random
+from datetime import datetime
 
 class Get_data(Set_data):
     # following function deprecated
@@ -6,34 +10,43 @@ class Get_data(Set_data):
         Set_data.make_mem_var(my_dict)
 
     def do_process(self):
+        seed(datetime.now())
+        value = random()
         value_00_dmod = {
-            "00_data_1":Set_data.data["a"],
-            "01_add":Set_data.data["a"]+ Set_data.data["b"],
-            "02_sub":Set_data.data["a"] - Set_data.data["c"]
+            "00_data":Set_data.data["a"],
+            "01_add":Set_data.data["a"]+ value*2,
+            "02_sub":Set_data.data["a"] - value*2
         }
 
         value_01_dmod = {
             "00_data":Set_data.data["b"],
-            "01_add":Set_data.data["b"] + Set_data.data["b"],
-            "02_sub":Set_data.data["b"] - Set_data.data["c"]
+            "01_add":Set_data.data["b"] + value*3,
+            "02_sub":Set_data.data["b"] - value*3
         }
 
-        demodulator = {
-            "value_00": value_00_dmod,
-            "value_01": value_01_dmod
-        }
+        # demodulator = {
+        #     "value_00": value_00_dmod,
+        #     "value_01": value_01_dmod
+        # }
+
+        demodulator = {}
+        keys = ["value_00","value_01"]
+        vals = [value_00_dmod, value_01_dmod]
+
+        for index,value in enumerate(keys):
+            demodulator[value] = vals[index]
 
 
         value_00_mod = {
-            "00_data_1":Set_data.data["a"],
-            "01_add":Set_data.data["a"] + Set_data.data["b"],
-            "02_sub":Set_data.data["a"] - Set_data.data["c"]
+            "00_data1":Set_data.data["a"],
+            "01_add1":Set_data.data["a"] + Set_data.data["b"],
+            "02_sub1":Set_data.data["a"] - Set_data.data["c"]
         }
 
         value_01_mod = {
-            "00_data":Set_data.data["b"],
-            "01_add":Set_data.data["b"] + Set_data.data["b"],
-            "02_sub":Set_data.data["b"] - Set_data.data["c"]
+            "00_data1":Set_data.data["b"],
+            "01_add1":Set_data.data["b"] + Set_data.data["b"],
+            "02_sub1":Set_data.data["b"] - Set_data.data["c"]
         }
 
         modulator = {
@@ -41,6 +54,23 @@ class Get_data(Set_data):
             "value_01": value_01_mod
         }
 
-        overall_status = {"Demodulator": demodulator, "Modulator": modulator}
+        value_00_dec = {
+            "00_data1":Set_data.data["a"],
+            "01_add1":Set_data.data["a"] + Set_data.data["b"],
+            "02_sub1":Set_data.data["a"] - Set_data.data["c"]
+        }
+
+        value_01_dec = {
+            "00_data1":Set_data.data["b"],
+            "01_add1":Set_data.data["b"] + Set_data.data["b"],
+            "02_sub1":Set_data.data["b"] - Set_data.data["c"]
+        }
+
+        decoder = {
+            "value_00": value_00_dec,
+            "value_01": value_01_dec
+        }
+
+        overall_status = {"Demodulator": demodulator, "Modulator": modulator, "Decoder":decoder}
 
         return overall_status
