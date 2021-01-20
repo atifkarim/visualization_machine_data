3cc77b8b584b83782fc6479c1f7e383d014ba230
atifkarim <atif0902082@gmail.com>
2021-01-21 00:23:39 +0100

    table name, position fixed

--- templates/static/js/build-table.js
+++ templates/static/js/build-table.js
@@ -94,8 +94,19 @@ function make_arr_little(tabledata) {
 // res_little = make_arr_little(tabledata[Object.keys(tabledata)[1]]);
 
 //create Tabulator on DOM element with id "example-table"
-var table = new Tabulator("#example-table", {
-    height: 205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+let table_1 = new Tabulator("#example-table", {
+    height: false, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+    data: [], //assign data to table
+    layout: "fitColumns", //fit columns to width of table (optional)
+    columns: [ //Define Table Columns
+        { title: "first col", field: "00_data", width: 150 },
+        { title: "second col", field: "01_add", hozAlign: "left" },
+        { title: "third col", field: "02_sub" },
+    ],
+});
+
+let table_2 = new Tabulator("#example-table_1", {
+    height: false, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
     data: [], //assign data to table
     layout: "fitColumns", //fit columns to width of table (optional)
     columns: [ //Define Table Columns
@@ -113,9 +124,11 @@ function table_with_tabular() {
         //     console.log("key: ", i, "data[i]: ", data[i][0]);
         // }s
         // table.setData(make_arr(data));
-        table.setData(make_arr_little(data[Object.keys(data)[1]]))
-            // table.render();
-            // console.log(data[Object.keys(data)[1]]);
+        table_1.setData(make_arr_little(data[Object.keys(data)[0]]));
+        table_2.setData(make_arr_little(data[Object.keys(data)[1]]));
+        // table_3.setData(make_arr_little(data[Object.keys(data)[2]]));
+        // table.render();
+        // console.log(data[Object.keys(data)[1]]);
 
     });
 }
@@ -125,3 +138,14 @@ $(document).ready(function() {
         $("#update_dict").toggle();
     });
 });
+
+function set_caption() {
+    $.get("/auto_update_table", function(data) {
+        // Object.keys(data)[0];
+        $(document).ready(function() {
+            $("#prop_1_span").text("Table " + Object.keys(data)[0]);
+            $("#prop_2_span").text("Table " + Object.keys(data)[1]);
+
+        });
+    });
+}
